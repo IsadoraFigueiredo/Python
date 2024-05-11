4ed16ba8e62d2654048f22ff95d5a41a980916cc
Isadora Figueiredo <148505640+IsadoraFigueiredo@users.noreply.github.com>
2024-05-11 11:22:47 -0300

    uso biblioteca geometria

---
+++ Biblioteca/teste.py
@@ -0,0 +1,9 @@
+# teste.py
+from geometria import Retangulo
+
+# Criando um objeto retângulo
+meu_retangulo = Retangulo(5, 10)
+
+# Testando os métodos
+print("\nÁrea do retângulo:", meu_retangulo.area())
+print("\nPerímetro do retângulo:", meu_retangulo.perimetro())
